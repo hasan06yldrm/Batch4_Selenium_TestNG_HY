eb96efd903e0339f22c04a7f3afd538200f58f96
ASUS <hasan06yildirim@hotmail.com>
2023-06-16 00:00:23 +0300

    added basic locators

---
+++ src/test/java/com/krafttech/tests/day03_webelement_intro/Homework.java
@@ -0,0 +1,39 @@
+package com.krafttech.tests.day03_webelement_intro;
+
+import java.util.Arrays;
+
+public class Homework {
+    public static void main(String[] args) {
+        String str = "red2 blue5 black1 green3 gold4";
+
+    }
+    //result-->black red green gold blue
+
+    public static String orderColors(String str) {
+        String[] words = str.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].substring(words[i].length() - 1) + words[i];
+        }
+        Arrays.sort(words);
+       // System.out.println("Arrays.toString(words) = " + Arrays.toString(words));
+
+        for (int i = 0; i < words.length; i++) {
+            words[i]=words[i].replace(words[i].substring(0,1), "");
+        }
+       // System.out.println("Arrays.toString(words) = " + Arrays.toString(words));
+
+      /*  String result="";
+        for (String word : words) {
+            result+=word+" ";
+        }
+        return result.trim(); */
+
+
+        String joined = String.join(" ", words);
+        return joined;
+    }
+}
+
+
+
