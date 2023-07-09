cc070a608f8464cea2aa6c98747ad23726ad6aac
ASUS <hasan06yildirim@hotmail.com>
2023-07-09 23:21:37 +0300

    add pom1

---
+++ src/test/java/com/krafttech/tests/TestBase.java
@@ -0,0 +1,29 @@
+package com.krafttech.tests;
+
+import com.krafttech.utilities.ConfigurationReader;
+import com.krafttech.utilities.Driver;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = Driver.get();
+        driver.manage().window().setPosition(new Point(-1000, 0));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfigurationReader.get("url"));
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        Driver.closeDriver();
+    }
+}
--- src/test/java/com/krafttech/utilities/WebDriverFactory.java
+++ src/test/java/com/krafttech/utilities/WebDriverFactory.java
@@ -3,6 +3,7 @@ package com.krafttech.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverFactory {
@@ -19,6 +20,27 @@ public class WebDriverFactory {
                 WebDriverManager.firefoxdriver().setup();
                 driver=new FirefoxDriver();
                 driver.manage().window().maximize();
+                break;
+        }
+        return driver;
+    }
+    public static WebDriver getDriver(){
+        WebDriver driver=null;
+
+        String browserType= BrowserName.browserName;
+
+        switch (browserType.toLowerCase()){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver=new ChromeDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver=new EdgeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver=new FirefoxDriver();
                 break;
         }
         return driver;
